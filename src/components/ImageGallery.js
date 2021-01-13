e8bdfa29424346ee8e26622fd0da08dd51d207b0
Angry-Hamster <krutsvanya@gmail.com>
2021-01-13 20:40:37 +0200

    fix prop types

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 import ImageGalleryItem from "./ImageGalleryItem";
 import Modal from "./Modal";
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 class Loader extends Component {
   render() {
